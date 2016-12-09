0f9c51a497ef0fe94d5352b8c15a280cd16c0d33
Sukora-Stas <stasiksukora@mail.ru>
2016-12-10 00:01:51 +0300

    test1

--- GoogleAPIroutes_GMap/Forms/Admin.cs
+++ GoogleAPIroutes_GMap/Forms/Admin.cs
@@ -1,16 +1,9 @@
 ﻿//подключаемые дерективы
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using GoogleAPIroutes_GMap.Forms;
 
-namespace GoogleAPIroutes_GMap
+using System;
+using System.Windows.Forms;
+
+namespace GoogleAPIroutes_GMap.Forms
 {
     public partial class Admin : Form
     {

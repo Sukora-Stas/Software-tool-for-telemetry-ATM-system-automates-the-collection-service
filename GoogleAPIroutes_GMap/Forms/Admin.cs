e836c1b291e3be34e399788a46f0ac06f19ac66a
Sukora-Stas <stasiksukora@mail.ru>
2016-12-10 00:03:16 +0300

    test2

--- GoogleAPIroutes_GMap/Forms/Admin.cs
+++ GoogleAPIroutes_GMap/Forms/Admin.cs
@@ -11,22 +11,7 @@ namespace GoogleAPIroutes_GMap.Forms
         {
             InitializeComponent();
         }
-        [Flags] //флаги для работы с формой
-enum AnimateWindowFlags
-{
-    AW_HOR_POSITIVE = 0x00000001,
-    AW_HOR_NEGATIVE = 0x00000002,
-    AW_VER_POSITIVE = 0x00000004,
-    AW_VER_NEGATIVE = 0x00000008,
-    AW_CENTER = 0x00000010,
-    AW_HIDE = 0x00010000,
-    AW_ACTIVATE = 0x00020000,
-    AW_SLIDE = 0x00040000,
-    AW_BLEND = 0x00080000
-}
-[System.Runtime.InteropServices.DllImport("user32.dll")]
         // дефолтная билиотека
-static  extern bool AnimateWindow(IntPtr hWnd, int time, AnimateWindowFlags flags);
         private void bankomatBindingNavigatorSaveItem_Click(object sender, EventArgs e)
         {
             //сохранение БД
